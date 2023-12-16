b80010a29383c47d8651c327eef948ba83fc4591
Mugen_Cyaegha <897657514@qq.com>
2023-12-16 17:34:15 +0800

    多线程15满分

--- proxy.c
+++ proxy.c
@@ -11,21 +11,62 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+typedef struct {
+    int connfd;
+} thread_args;
+
+void *thread(void *vargp) {
+    pthread_detach(pthread_self());
+    thread_args *argp;
+    size_t read_len, write_len;
+    int facing_server_fd;
+    
+    HTTPRequest request;
+    char *real_request;
+    char *response_buf;
+
+    argp = (thread_args *)vargp;
+
+    request_init(&request);
+    if(read_request(argp->connfd, &request) == -1) {
+        fprintf(stderr, "Malformed Httprequest\n");
+    }
+    
+    facing_server_fd = open_clientfd(request.host, request.port);
+    real_request = malloc(sizeof(char) * MAXLINE);
+    if ((write_len = construct_real_request(&request, real_request)) == -1) {
+        fprintf(stderr, "Fail to construct real request\n");
+    };
+    printf("Thread[%d]: received %ld bytes from client\n", pthread_self(), read_len);
+    send_request(facing_server_fd, real_request, write_len);
+    free(real_request);
+    real_request = NULL;
+    request_clear(&request);
+
+    response_buf = malloc(sizeof(char) * MAXLINE);
+    if ((response_buf = read_response(facing_server_fd, response_buf, &read_len)) == -1) {
+        fprintf(stderr, "Fail to construct real request\n");
+    }
+    printf("Thread[%d]: received %ld bytes from server\n", pthread_self(), read_len);
+    send_response(argp->connfd, response_buf, read_len);
+    free(response_buf);
+    response_buf = NULL;
+    close(argp->connfd);
+    free(argp);
+    return NULL;
+}
+
 int main(int argc, char **argv)
 {
     /* 忽略SIGPIPE信号 */
     signal(SIGPIPE, SIG_IGN);
 
-    int listenfd, connfd, facing_server_fd;
+    int listenfd, connfd;
+    char client_hostname[MAXLINE], client_port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
-    char client_hostname[MAXLINE], client_port[MAXLINE];
-    // char server_hostname[MAXLINE], server_port[MAXLINE];
-    int read_request_result;
-    HTTPRequest request;
-    char *real_request;
-    char *response_buf;
-    size_t write_len, read_len;
+
+    pthread_t tid;
 
     /* 检查命令行参数 */
     if (argc != 2) {
@@ -37,48 +78,13 @@ int main(int argc, char **argv)
     listenfd = open_listenfd(argv[1]);
     while(1) {
         clientlen = sizeof(clientaddr);
-        /* 获取一个连接套接字描述符, 客户端信息保存在clientaddr里面 */
         connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
-        /* 从clientaddr里面解析出host和port */
         getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
-        dbg_printf("Proxy[%d]: with client connfd: %d\n", __LINE__, connfd);
-        dbg_printf("Proxy[%d]: ready to read request from client\n", __LINE__);
-        /* 从客户端读取请求 */
-        request_init(&request);
-        if(read_request(connfd, &request) == -1) {
-            fprintf(stderr, "Malformed Httprequest\n");
-        }
-        
-        facing_server_fd = open_clientfd(request.host, request.port);
-        dbg_printf("Proxy[%d]: facing_server_fd: %d\n", __LINE__, facing_server_fd);
-        real_request = malloc(sizeof(char) * MAXLINE);
-        if ((write_len = construct_real_request(&request, real_request)) == -1) {
-            fprintf(stderr, "Fail to construct real request\n");
-        };
-        dbg_printf("Proxy[%d]: real_request: %s\n", __LINE__, real_request);
-        /* 向服务器发送http请求 */
-        printf("Proxy[%d]: received %ld bytes from client\n", __LINE__, write_len);
-        send_request(facing_server_fd, real_request, write_len);
-        dbg_printf("Proxy[%d]: sent request to server\n", __LINE__);
-        free(real_request);
-        real_request = NULL;
-        request_clear(&request);
 
-
-        response_buf = malloc(sizeof(char) * MAXLINE);
-        dbg_printf("Proxy[%d]: ready to read response from server\n", __LINE__);
-        dbg_printf("response_buf: %p\n", response_buf);
-        if ((response_buf = read_response(facing_server_fd, response_buf, &read_len)) == -1) {
-            fprintf(stderr, "Fail to construct real request\n");
-        }
-        printf("Proxy[%d]: received %ld bytes from server\n", __LINE__, read_len);
-        dbg_printf("response_buf: %p\n", response_buf);
-        send_response(connfd, response_buf, read_len);
-        dbg_printf("Proxy[%d]: response sent back to client\n", __LINE__);
-        free(response_buf);
-        dbg_printf("Proxy[%d]: response_buf freed explicitly\n", __LINE__);
-        response_buf = NULL;
-        close(connfd);
+        /* 放到堆上 */
+        thread_args *arg = malloc(sizeof(thread_args));
+        arg->connfd = connfd;
+        pthread_create(&tid, NULL, thread, arg);
     }
     return 0;
 }
